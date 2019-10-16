aa31e5da7f566e143ad8cd8df90db77516d1c7a9
Ryan LeFevre <meltingice8917@gmail.com>
2019-10-16 18:13:06 -0400

    Clean up accounts controller by moving account validation to a plug

--- lib/nanocrawler_web/controllers/api/v2/accounts_controller.ex
+++ lib/nanocrawler_web/controllers/api/v2/accounts_controller.ex
@@ -5,169 +5,151 @@ defmodule NanocrawlerWeb.Api.V2.AccountsController do
   import Nanocrawler.Cache
   import Nanocrawler.Util, only: [account_is_valid?: 1, timestampForBlock: 1]
 
+  plug :validate_account
+
   def show(conn, %{"account" => account}) do
-    cond do
-      account_is_valid?(account) ->
-        rpc_data =
-          fetch("v2/account/#{account}", 10, fn ->
-            NanoAPI.rpc("account_info", %{
-              account: account,
-              representative: true,
-              weight: true,
-              pending: true
-            })
-          end)
+    rpc_data =
+      fetch("v2/account/#{account}", 10, fn ->
+        NanoAPI.rpc("account_info", %{
+          account: account,
+          representative: true,
+          weight: true,
+          pending: true
+        })
+      end)
 
-        case rpc_data do
-          {:ok, data} ->
-            json(conn, %{account: data})
+    case rpc_data do
+      {:ok, data} ->
+        json(conn, %{account: data})
 
-          {:error, "Bad account number" = data} ->
-            conn |> put_status(:bad_request) |> json(%{error: data})
+      {:error, "Bad account number" = data} ->
+        conn |> put_status(:bad_request) |> json(%{error: data})
 
-          {:error, "Account not found" = data} ->
-            conn |> put_status(:not_found) |> json(%{error: data})
+      {:error, "Account not found" = data} ->
+        conn |> put_status(:not_found) |> json(%{error: data})
 
-          {:error, msg} ->
-            conn |> put_status(500) |> json(%{error: msg})
-        end
-
-      true ->
-        conn |> put_status(:bad_request) |> json(CommonErrors.account_invalid())
+      {:error, msg} ->
+        conn |> put_status(500) |> json(%{error: msg})
     end
   end
 
   def weight(conn, %{"account" => account}) do
-    cond do
-      account_is_valid?(account) ->
-        rpc_data =
-          fetch("v2/account/#{account}/weight", 300, fn ->
-            NanoAPI.rpc("account_weight", %{account: account})
-          end)
+    rpc_data =
+      fetch("v2/account/#{account}/weight", 300, fn ->
+        NanoAPI.rpc("account_weight", %{account: account})
+      end)
 
-        case rpc_data do
-          {:ok, data} -> json(conn, %{weight: data})
-          {:error, msg} -> conn |> put_status(500) |> json(%{error: msg})
-        end
-
-      true ->
-        conn |> put_status(:bad_request) |> json(CommonErrors.account_invalid())
+    case rpc_data do
+      {:ok, data} -> json(conn, %{weight: data})
+      {:error, msg} -> conn |> put_status(500) |> json(%{error: msg})
     end
   end
 
   def delegators(conn, %{"account" => account}) do
-    cond do
-      account_is_valid?(account) ->
-        rpc_data =
-          fetch("v2/account/#{account}/delegators", 300, fn ->
-            NanoAPI.rpc("delegators", %{account: account})
-          end)
+    rpc_data =
+      fetch("v2/account/#{account}/delegators", 300, fn ->
+        NanoAPI.rpc("delegators", %{account: account})
+      end)
 
-        case rpc_data do
-          {:ok, data} -> json(conn, data)
-          {:error, msg} -> conn |> put_status(500) |> json(%{error: msg})
-        end
-
-      true ->
-        conn |> put_status(:bad_request) |> json(CommonErrors.account_invalid())
+    case rpc_data do
+      {:ok, data} -> json(conn, data)
+      {:error, msg} -> conn |> put_status(500) |> json(%{error: msg})
     end
   end
 
   def history(conn, %{"account" => account} = params) do
-    cond do
-      account_is_valid?(account) ->
-        rpc_data =
-          fetch("v2/account/#{account}/history/#{params["head"] || ""}", 10, fn ->
-            NanoAPI.rpc("account_history", %{
-              account: account,
-              count: 50,
-              raw: true,
-              head: params["head"]
-            })
+    rpc_data =
+      fetch("v2/account/#{account}/history/#{params["head"] || ""}", 10, fn ->
+        NanoAPI.rpc("account_history", %{
+          account: account,
+          count: 50,
+          raw: true,
+          head: params["head"]
+        })
+      end)
+
+    case rpc_data do
+      {:ok, %{"history" => ""}} ->
+        conn |> put_status(:not_found) |> json(CommonErrors.account_not_found())
+
+      {:ok, %{"history" => history}} ->
+        history =
+          Enum.map(history, fn entry ->
+            Map.merge(entry, %{"timestamp" => timestampForBlock(entry["hash"])})
           end)
 
-        case rpc_data do
-          {:ok, %{"history" => ""}} ->
-            conn |> put_status(:not_found) |> json(CommonErrors.account_not_found())
+        json(conn, %{"history" => history})
 
-          {:ok, %{"history" => history}} ->
-            history =
-              Enum.map(history, fn entry ->
-                Map.merge(entry, %{"timestamp" => timestampForBlock(entry["hash"])})
-              end)
+      {:error, "Bad account number" = data} ->
+        conn |> put_status(:bad_request) |> json(%{error: data})
 
-            json(conn, %{"history" => history})
-
-          {:error, "Bad account number" = data} ->
-            conn |> put_status(:bad_request) |> json(%{error: data})
-
-          {:error, msg} ->
-            conn |> put_status(500) |> json(%{error: msg})
-        end
-
-      true ->
-        conn |> put_status(:bad_request) |> json(CommonErrors.account_invalid())
+      {:error, msg} ->
+        conn |> put_status(500) |> json(%{error: msg})
     end
   end
 
   def pending(conn, %{"account" => account}) do
-    cond do
-      account_is_valid?(account) ->
-        data =
-          fetch("v2/account/#{account}/pending", 10, fn ->
-            rpc_data =
-              NanoAPI.rpc("accounts_pending", %{
-                accounts: [account],
-                source: true,
-                # 0.000001
-                threshold: "1000000000000000000000000",
-                sorting: true
-              })
+    data =
+      fetch("v2/account/#{account}/pending", 10, fn ->
+        rpc_data =
+          NanoAPI.rpc("accounts_pending", %{
+            accounts: [account],
+            source: true,
+            # 0.000001
+            threshold: "1000000000000000000000000",
+            sorting: true
+          })
 
-            case rpc_data do
-              {:ok, %{"blocks" => accounts}} ->
-                # Since we're really only fetching 1 account, we can just grab the only entry in the Map.
-                all_blocks = accounts[Map.keys(accounts) |> hd]
+        case rpc_data do
+          {:ok, %{"blocks" => accounts}} ->
+            # Since we're really only fetching 1 account, we can just grab the only entry in the Map.
+            all_blocks = accounts[Map.keys(accounts) |> hd]
 
-                # Because some accounts can have a ton of pending transactions, we're only interested in
-                # the first 20.
-                blocks =
-                  all_blocks
-                  |> Map.to_list()
-                  |> Enum.slice(0, 20)
-                  |> Enum.into(%{})
-                  |> Enum.map(fn {hash, block} ->
-                    %{
-                      type: "pending",
-                      amount: block["amount"],
-                      hash: hash,
-                      source: block["source"],
-                      timestamp: timestampForBlock(hash)
-                    }
-                  end)
+            # Because some accounts can have a ton of pending transactions, we're only interested in
+            # the first 20.
+            blocks =
+              all_blocks
+              |> Map.to_list()
+              |> Enum.slice(0, 20)
+              |> Enum.into(%{})
+              |> Enum.map(fn {hash, block} ->
+                %{
+                  type: "pending",
+                  amount: block["amount"],
+                  hash: hash,
+                  source: block["source"],
+                  timestamp: timestampForBlock(hash)
+                }
+              end)
 
-                case NanoAPI.rpc("account_balance", %{account: account}) do
-                  {:ok, %{"pending" => pending_balance}} ->
-                    {:ok,
-                     %{
-                       total: map_size(all_blocks),
-                       blocks: blocks,
-                       pendingBalance: pending_balance
-                     }}
+            case NanoAPI.rpc("account_balance", %{account: account}) do
+              {:ok, %{"pending" => pending_balance}} ->
+                {:ok,
+                 %{
+                   total: map_size(all_blocks),
+                   blocks: blocks,
+                   pendingBalance: pending_balance
+                 }}
 
-                  {:error, msg} ->
-                    {:error, msg}
-                end
-
-              true ->
-                rpc_data
+              {:error, msg} ->
+                {:error, msg}
             end
-          end)
 
-        case data do
-          {:ok, resp} -> json(conn, resp)
-          {:error, msg} -> conn |> put_status(500) |> json(%{error: msg})
+          true ->
+            rpc_data
         end
+      end)
+
+    case data do
+      {:ok, resp} -> json(conn, resp)
+      {:error, msg} -> conn |> put_status(500) |> json(%{error: msg})
+    end
+  end
+
+  def validate_account(conn, _) do
+    cond do
+      account_is_valid?(conn.params["account"]) ->
+        conn
 
       true ->
         conn |> put_status(:bad_request) |> json(CommonErrors.account_invalid())
