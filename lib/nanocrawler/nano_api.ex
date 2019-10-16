d1e66709963add122fff6f47fb241c03a899af9f
Ryan LeFevre <meltingice8917@gmail.com>
2019-10-15 21:35:08 -0400

    Filter nil values from RPC request

--- lib/nanocrawler/nano_api.ex
+++ lib/nanocrawler/nano_api.ex
@@ -1,5 +1,7 @@
 defmodule Nanocrawler.NanoAPI do
   def rpc(action, options \\ %{}) do
+    # Filter nil values
+    options = options |> Enum.filter(fn {_, v} -> !is_nil(v) end) |> Enum.into(%{})
     request_body = Map.merge(%{action: action}, options) |> Jason.encode!()
 
     resp =
